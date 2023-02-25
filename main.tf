88d9111caf13bd61702d856236b0c0fc7d26b710
hugoivanzuloagaaguilar <81063697+hugoivanzuloagaaguilar@users.noreply.github.com>
2023-02-24 21:36:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,21 +13,141 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-module "eks" {
-  source  = "punkerside/eks/aws"
-  version = "0.0.5"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.3"
 
-  name                    = var.name
-  eks_version             = var.eks_version
-  desired_size            = var.desired_size
-  max_size                = var.max_size
-  min_size                = var.min_size
-  instance_types          = [var.instance_types]
-  disk_size               = var.disk_size
-  ami_type                = var.ami_type
-  force_update_version    = true
-  endpoint_private_access = false
-  endpoint_public_access  = true
-  subnet_private_ids      = var.subnet_private_ids
-  subnet_public_ids       = var.subnet_public_ids
+  name = "main"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = "staging"
+  }
 }
+  
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "my-eks"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = {
+    Environment = "staging"
+  }
+}
+    module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "my-eks"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 10
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = {
+    Environment = "staging"
+  }
+}
+    
